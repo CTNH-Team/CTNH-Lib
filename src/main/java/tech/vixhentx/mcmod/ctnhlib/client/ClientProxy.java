323d85f2b2fecd0942dc21fd0f6431b9654ef25f
moguang <184676648@qq.com>
2026-01-19 18:21:30 +0800

    高亮

--- src/main/java/tech/vixhentx/mcmod/ctnhlib/client/ClientProxy.java
+++ src/main/java/tech/vixhentx/mcmod/ctnhlib/client/ClientProxy.java
@@ -1,9 +1,12 @@
 package tech.vixhentx.mcmod.ctnhlib.client;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import tech.vixhentx.mcmod.ctnhlib.CTNHLib;
+import tech.vixhentx.mcmod.ctnhlib.client.render.highlight.HighlightRender;
 import tech.vixhentx.mcmod.ctnhlib.common.CommonProxy;
 
 @Mod.EventBusSubscriber(modid = CTNHLib.MODID,bus = Mod.EventBusSubscriber.Bus.FORGE,value = Dist.CLIENT)
@@ -14,5 +17,10 @@ public class ClientProxy extends CommonProxy {
     }
     public static void init() {
 
+    }
+    @SubscribeEvent
+    public static void onRenderLevel(RenderLevelStageEvent event) {
+        // 调用HighlightRender的hook方法，执行高亮渲染逻辑
+        HighlightRender.hook(event);
     }
 }
---
+++ src/main/java/tech/vixhentx/mcmod/ctnhlib/utils/ExtendNbtUtils.java
@@ -0,0 +1,29 @@
+package tech.vixhentx.mcmod.ctnhlib.utils;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Quaternionf;
+
+public class ExtendNbtUtils {
+    public static CompoundTag writeVec3(Vec3 vec3) {
+        CompoundTag compoundtag = new CompoundTag();
+        compoundtag.putDouble("X", vec3.x);
+        compoundtag.putDouble("Y", vec3.y);
+        compoundtag.putDouble("Z", vec3.z);
+        return compoundtag;
+    }
+    public static Vec3 readVec3(CompoundTag nbt) {
+        return new Vec3(nbt.getDouble("X"), nbt.getDouble("Y"), nbt.getDouble("Z"));
+    }
+    public static CompoundTag writeQuaternionf(Quaternionf quaternionf) {
+        CompoundTag compoundtag = new CompoundTag();
+        compoundtag.putFloat("RotationX", quaternionf.x());
+        compoundtag.putFloat("RotationY", quaternionf.y());
+        compoundtag.putFloat("RotationZ", quaternionf.z());
+        compoundtag.putFloat("RotationW", quaternionf.w());
+        return compoundtag;
+    }
+    public static Quaternionf readQuaternionf(CompoundTag nbt) {
+        return new Quaternionf(nbt.getFloat("RotationX"), nbt.getFloat("RotationY"), nbt.getFloat("RotationZ"), nbt.getFloat("RotationW"));
+    }
+}
