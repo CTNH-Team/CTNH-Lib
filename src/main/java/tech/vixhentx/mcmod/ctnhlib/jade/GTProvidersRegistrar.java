a75c08a627253470162035bddf4ba8e1eb49861d
Li Silin <2667023708@qq.com>
2026-08-07 23:47:57 +0800

    feat&fix: 修复/调整了花园配方 修复/调整了永恒苦难之井 初步增加宝石切割机，干掉了神话的硬编码代码换成自己的builder 调整了永恒苦难之井的结构，它的舱室只能放在 大 血 石 砖 上

--- src/main/java/tech/vixhentx/mcmod/ctnhlib/jade/GTProvidersRegistrar.java
+++ src/main/java/tech/vixhentx/mcmod/ctnhlib/jade/GTProvidersRegistrar.java
@@ -24,8 +24,8 @@ public final class GTProvidersRegistrar {
         JadePriorityManager.registerBlockData(new WorkLogicMachineProvider(), BlockEntity.class, 1200, "workable_data");
         JadePriorityManager.registerBlockData(new ControllableBlockProvider(), BlockEntity.class, 1300,
                 "controllable_data");
-        // JadePriorityManager.registerBlockData(new RecipeLogicProvider(), BlockEntity.class, 1400,
-        // "recipe_logic_data");
+         JadePriorityManager.registerBlockData(new RecipeLogicProvider(), BlockEntity.class, 1400,
+         "recipe_logic_data");
         JadePriorityManager.registerBlockData(new ParallelProvider(), BlockEntity.class, 1500, "parallel_data");
         JadePriorityManager.registerBlockData(new RecipeOutputProvider(), BlockEntity.class, 1600,
                 "recipe_output_data");
